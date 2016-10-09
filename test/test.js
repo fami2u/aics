06e75857e9d625e0136e0e40a7be1ddbe398ade3
qintengfei <devqin@gmail.com>
2016-10-09 14:34:37 +0800

    change file && add first unit test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,17 @@
 import test from 'ava';
+import assert from 'assert'
 
-test('foo', t => {
-    t.pass();
+import Account from '../lib/account';
+import Package from '../lib/package';
+import Project from '../lib/project';
+
+
+/**
+ * account.js
+ */
+
+test('checkUsername', t => {
+	var usernameReg = /^\w+[\w\s]+\w+$/;
+  assert(usernameReg.test("qin123") === true)
 });
 
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-
-    t.is(await bar, 'bar');
-});
